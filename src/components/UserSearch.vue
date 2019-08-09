a0cafe2127ccb500dfd3b532108591f7c8966e87
tracyalison11 <tracyak13@gmail.com>
2019-08-09 01:17:39 -0400

    created empty state screen

--- src/components/UserSearch.vue
+++ src/components/UserSearch.vue
@@ -1,43 +1,47 @@
 <template>
-  <div class="search">
+  <div>
     <div class="search-box">
       <div class="search-circle" v-bind:class="{ active: isActive }">
         <img src="../assets/search-icon.svg" class="search-icon" alt="magnifying glass" @mouseover="showInput"/>
         <input class="search-input" v-bind:class="{ active: isActive }" type="text" placeholder="looking for someone?" v-on:keyup="searchForGitHubUsers"/>
-        <span></span>
       </div>
     </div>
 
-      <p class="search-results-info error" v-if="error && !loaded">An error occurred during search. You may have exceeded the rate limit. Please try again.</p>
-      <p class="search-results-info" v-if="loaded">Total search results: {{Number(this.totalResults).toLocaleString()}}</p>
+    <p class="search-results-info error" v-if="error && !loaded">An error occurred during search. You may have exceeded the rate limit. Please wait and try again.</p>
+    <p class="search-results-info" v-if="loaded">Total search results: {{Number(this.totalResults).toLocaleString()}}</p>
 
-      <div class="search-results-table" v-if="loaded">
-        <p class="search-results-info">Current Page: {{ currentPage }}</p>
-        <div class="overflow-auto">
-          <b-pagination
-            v-model="currentPage"
-            :total-rows="rows"
-            :per-page="perPage"
-            aria-controls="my-table"
-            align="center"
-          ></b-pagination>
-        </div>
-        <b-table
-          id="my-table"
-          :items="users"
-          :fields="fields"
+    <div class="empty-screen" v-if="!loaded">
+      <img src="../assets/inspectocat.jpg" class="inspectocat" alt="inspector character"/>
+      <p class="directions">Mouse over the search icon to look for friends!</p>
+    </div>
+
+    <div class="search-results-table" v-if="loaded">
+      <p class="search-results-info">Current Page: {{ currentPage }}</p>
+      <div class="overflow-auto">
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="rows"
           :per-page="perPage"
-          @row-clicked="rowClicked"
-          :current-page="currentPage"
-          small
-        >
-          <!-- A virtual column to show avatar -->
-          <template slot="Avatar" slot-scope="data">
-            <img class="avatar" :src="`${data.item.avatar_url}`" />
-          </template>
-        </b-table>
+          aria-controls="my-table"
+          align="center"
+        ></b-pagination>
       </div>
+      <b-table
+        id="my-table"
+        :items="users"
+        :fields="fields"
+        :per-page="perPage"
+        @row-clicked="rowClicked"
+        :current-page="currentPage"
+        small
+      >
+        <!-- A virtual column to show avatar -->
+        <template slot="Avatar" slot-scope="data">
+          <img class="avatar" :src="`${data.item.avatar_url}`" />
+        </template>
+      </b-table>
     </div>
+  </div>
 </template>
 
 <script>
@@ -56,7 +60,7 @@ export default {
       error: false,
       isActive: false
     }
-  }
+  },
   methods: {
     searchForGitHubUsers: function() {
       var searchElement = $('.search-input');
